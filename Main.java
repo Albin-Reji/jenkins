bcff27d6ec0995a36cc9c7d4a7611413f636551b
Albin Reji <albinrejim30@gmail.com>
2025-11-06 10:40:58 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("This is Java Main class ");
+        System.out.println("test: jenkin pipeline");
+        System.err.println(new Date());
+    }
+}
