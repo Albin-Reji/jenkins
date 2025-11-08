646f5e0f7038f9efe8f4805e4608f938f9c0d596
Albin Reji <albinrejim30@gmail.com>
2025-11-08 20:07:30 +0530

    docker file

--- Main.java
+++ Main.java
@@ -2,14 +2,6 @@ import java.util.Date;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("This is Java Main class ");
-        System.out.println("test: jenkin pipeline");
-        System.err.println(new Date());
-        getJenkins();
-        Init init=new Init("Albin");
-        System.err.println(init.getName());
-    }
-    public static void getJenkins(){
-        System.out.println("Jenkin Project Executed...");
+        System.out.println("🚀 Hello from Dockerized Java App!");
     }
 }
